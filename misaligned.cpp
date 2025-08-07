ad555473f3779f36894a73667d94876440279809
Malavika-Anil <malavikaanil9891@gmail.com>
2025-08-07 10:19:45 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,21 +1,31 @@
 #include <iostream>
 #include <assert.h>
 
-int printColorMap() {
+int printColorMap(int &totalPairs) {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-    int i = 0, j = 0;
-    for(i = 0; i < 5; i++) {
-        for(j = 0; j < 5; j++) {
-            std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
+    int count = 0;
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            std::cout << count << " | " << majorColor[i] << " | " << minorColor[j] << "\n";
+            count++;
         }
     }
-    return i * j;
+    totalPairs = count;
+    return 0;  
 }
 
 void testPrintColorMap() {
-    std::cout << "\nPrint color map test\n"; 
-    int result = printColorMap();
-    assert(result == 25);
+    std::cout << "\nPrint color map test\n";
+    int total = 0;
+    int ret = printColorMap(total);
+    
+    assert(total == 25);
+    assert(ret == 0);
     std::cout << "All is well (maybe!)\n";
+}
+
+int main() {
+    testPrintColorMap();
+    return 0;
 }
