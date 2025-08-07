8984a22682e4c1d3533454fa8efebccb1a7f8b65
Malavika-Anil <malavikaanil9891@gmail.com>
2025-08-07 20:36:03 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -4,32 +4,37 @@
 #include <cassert>
 
 std::vector<std::string> getColorMap() {
-    static const std::vector<std::string> major = {"White","Red","Black","Yellow","Violet"};
-    static const std::vector<std::string> minor = {"Blue","Orange","Green","Brown","Slate"};
-    std::vector<std::string> map;
-    map.reserve(25);
+    const std::vector<std::string> major = {"White", "Red", "Black", "Yellow", "Violet"};
+    const std::vector<std::string> minor = {"Blue", "Orange", "Green", "Brown", "Slate"};
+    std::vector<std::string> colorMap;
+    colorMap.reserve(25);
 
     for (int i = 0; i < 5; ++i) {
         for (int j = 0; j < 5; ++j) {
-            map.push_back(
-                std::to_string(i * 5 + j) + " | " +
-                major[i] + " | " + minor[j]
+            colorMap.push_back(
+                std::to_string(i * 5 + j) + " | " + major[i] + " | " + minor[i]  // BUG: minor[i] should be minor[j]
             );
         }
     }
-    return map;
+    return colorMap;
+}
+
+int printColorMap() {
+    const auto& colorMap = getColorMap();
+    for (const auto& entry : colorMap) {
+        std::cout << entry << "\n";
+    }
+    return static_cast<int>(colorMap.size());
 }
 
 void testPrintColorMap() {
     std::cout << "\nPrint color map test\n";
     auto colorMap = getColorMap();
-    assert(colorMap.size() == 25);
 
-   
-    assert(colorMap[0] == "0 | White | Blue");
-    assert(colorMap[4] == "4 | White | Slate");
-    assert(colorMap[5] == "5 | Red | Blue");
-    assert(colorMap[24] == "24 | Violet | Slate");
+    assert(colorMap.size() == 25);
+    assert(colorMap[0] == "0 | White | Blue");      // Will fail
+    assert(colorMap[6] == "6 | Red | Green");       // Will fail
+    assert(colorMap[24] == "24 | Violet | Slate");  // Will fail
 
     std::cout << "All is well (maybe!)\n";
 }
@@ -38,4 +43,3 @@ int main() {
     testPrintColorMap();
     return 0;
 }
-
